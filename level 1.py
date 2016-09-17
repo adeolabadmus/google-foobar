5bee3576b079fb9d6d42202c51e7aab453bf7c2c
Adeola Badmus <badmusad@gmail.com>
2016-09-17 15:55:55 +0100

    Add comments and remove in-module tests

--- level 1.py
+++ level 1.py
@@ -1,11 +1,14 @@
 def answer(s):
+    # for each character in string s, call dec function dec if character
+    #  is  lower case, else just use as-is
     ans = [dec(i) if i.islower() else i for i in s]
     return ''.join(ans)
 
 
 def dec(s):
+    # convert string to its ascii value, and deduct from 'a'
     o = ord(s) - 97
+    # subtract from 'z' to determine new value
     n = 122 - o
+    # return ascii character
     return chr(n)
-
-print answer('vmxibkgrlm')
