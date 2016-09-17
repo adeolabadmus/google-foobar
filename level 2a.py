d4ba6caf45d1362d5486e039fcb77059f87dcdc2
Adeola Badmus <badmusad@gmail.com>
2016-09-17 16:47:55 +0100

    Add comments and remove in-module tests

--- level 2a.py
+++ level 2a.py
@@ -1,7 +1,11 @@
-import sys
+def answer(n, b):
+    restrict_values(n, b)
+    get_length.results = []  # set memoizer to store computed results
+    return get_length(n, b)
 
 
 def to_base(base, number):
+    '''This function converts from base 10 to any other base'''
     base = int(base)
     number = int(number)
     rems = ''
@@ -11,17 +15,20 @@ def to_base(base, number):
         number = number/base
         if number == 0:
             break
-    result = rems[::-1]
+    result = rems[::-1]  # reverse remainders
     return int(result)
 
 
 def from_base(base, number):
+    '''This function converts to base 10 from any base'''
     base = int(base)
     number = str(number)
     return int(number, base)
 
 
 def subtract(tor, tee, base):
+    '''This function subtracts two numbers, 'subtractor'
+    and 'subractee' in the given base'''
     tor = int(tor)
     tee = int(tee)
     if base == 10:
@@ -32,6 +39,8 @@ def subtract(tor, tee, base):
 
 
 def restrict_values(n, b):
+    '''This function restricts values of 'k' and 'b',
+    as stated in the question'''
     k = len(n)
     b = int(b)
     k_okay = (k >= 2) and (k <= 9)
@@ -46,35 +55,31 @@ def restrict_values(n, b):
 
 
 def get_length(n, b):
-    y = ''.join(sorted(n))
-    x = y[::-1]
+    '''This function does the actual work'''
+    y = ''.join(sorted(n))  # sort 'n' and assign to 'y'
+    x = y[::-1]  # reverse 'y' to get decreasing order, 'x'
 
     diff = subtract(x, y, b)
-    z_ = list(str(diff))
+    z_ = list(str(diff))  # convert diff between 'x' and 'y' to list
 
+    # pad z as necessary
     offset = len(n) - len(z_)
     if offset:
         for i in range(offset):
             z_.insert(0, '0')
     z = ''.join(z_)
-    if int(z) == 0:
+
+    if int(z) == 0:  # if answer is 0, return 1 as length
         return 1
     else:
-        if z in get_length.results:
+        if z in get_length.results:  # if 'z' is already in results
+            # if z is the last element, then length is 1
             if get_length.results[-1] == z:
                 return 1
             else:
+                # get distance between previous and present z
                 z_index = get_length.results.index(z)
                 return len(get_length.results) - z_index
         else:
             get_length.results.append(z)
     return get_length(z, b)
-
-
-def answer(n, b):
-    restrict_values(n, b)
-    get_length.results = []
-    print get_length(n, b)
-
-if __name__ == '__main__':
-    answer(sys.argv[1], sys.argv[2])
--- level 2b.py
+++ level 2b.py
@@ -10,15 +10,19 @@ def answer(x, y):
 def get_id(x, y):
     if y == 1:
         return sigma(x)
+    # solve problem recursively using formula (x,y) = (x,y-1)+y+(x-2)
     return get_id(x, y - 1) + y + (x - 2)
 
 
 # Iterative Solution
 def get_id_(x, y, acc=0):
+    '''This function converts the recursive solution
+    to iteration'''
     while y > 1:
         x, y, acc = x, y-1, acc + y + x - 2
     return acc + sigma(x)
 
 
 def sigma(n):
+    '''return summation from 1 to n using gauss trick'''
     return 1 if n == 1 else n * (n + 1) / 2
